c31ab1ef89c001aa31e63e951ca072d25a3fda9f
Simon Fernandez <simon.fernandez@ens-lyon.fr>
2020-12-20 23:21:21 +0100

    Small bot

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+use serenity::client::{Client, Context, EventHandler};
+use serenity::framework::standard::{
+    macros::{command, group},
+    CommandResult, StandardFramework,
+};
+use serenity::model::channel::Message;
+use serenity::{async_trait, framework::standard::Args};
+
+use std::fs;
+
+#[group]
+#[commands(angry)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("!")) // set the bot's prefix to "~"
+        .group(&GENERAL_GROUP);
+
+    // Login with a bot token from the environment
+    let token = fs::read_to_string("DISCORD_TOKEN").expect("Error when reading the token");
+
+    let mut client = Client::builder(token)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn angry(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let (n, emoji) = match args.single::<i64>()? {
+        x if x > 0 => (x, "<:raaa:781638510390018089>"),
+        x if x < 0 => (-x, "<:surexcite:781248874253516823>"),
+        0 => (1, "<:squint:779480843310989342>"),
+        _ => return Err("Error parsing the number".into()),
+    };
+
+    let content: String = (0..n).map(|_| emoji).collect::<Vec<_>>().join(" ");
+
+    msg.channel_id.say(ctx, content).await?;
+
+    Ok(())
+}
