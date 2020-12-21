17f3e941ca24312c8034497e9d01835f79a5e696
Simon Fernandez <simon.fernandez@ens-lyon.fr>
2020-12-21 13:46:14 +0100

    Fix min value

--- src/main.rs
+++ src/main.rs
@@ -41,8 +41,8 @@ async fn main() {
 #[command]
 async fn angry(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
     let (n, emoji) = match args.single::<i64>()? {
-        x if x > 0 => (max(x, 50), "<:raaa:781638510390018089>"),
-        x if x < 0 => (max(-x, 50), "<:surexcite:781248874253516823>"),
+        x if x > 0 => (min(x, 50), "<:raaa:781638510390018089>"),
+        x if x < 0 => (min(-x, 50), "<:surexcite:781248874253516823>"),
         0 => (1, "<:squint:779480843310989342>"),
         _ => return Err("Error parsing the number".into()),
     };
